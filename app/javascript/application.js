7c7426d2132c3d27a38dc22c209f589f283c2c59
ParamagicDev <konnor5456@gmail.com>
2021-10-16 19:54:50 -0400

    feat: add shoelace

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import { setBasePath } from '@shoelace-style/shoelace';
+setBasePath("https://unpkg.com/@shoelace-style/shoelace@2.0.0-beta.57/dist/")
+
